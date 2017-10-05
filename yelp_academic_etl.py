e74856e0f1f94c8b7d857e3ec81a6eb50b8b90f9
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-05 12:51:19 -0500

    fix white space

--- yelp_academic_etl.py
+++ yelp_academic_etl.py
@@ -2,40 +2,40 @@ import io
 
 import mysql.connector
 
-# from sklearn.feature_extraction.text 
+# from sklearn.feature_extraction.text
 
 
 def write_document(cursor, cat):
-	""" Given a yelp category, build out a text document
-	which has all the reviews for that category """
-	query = ("SELECT review.text "
-			 "FROM review INNER JOIN category ON review.business_id = category.business_id "
-			 "WHERE category.category=%s")
-	cursor.execute(query, (cat,))
+    """ Given a yelp category, build out a text document
+    which has all the reviews for that category """
+    query = ("SELECT review.text "
+             "FROM review INNER JOIN category ON review.business_id = category.business_id "
+             "WHERE category.category=%s")
+    cursor.execute(query, (cat,))
 
-	n_encoding_errors = 0
-	n_review = 0
-	with io.open("reviewtext/%s.txt" % cat, 'w', encoding='utf8') as f:
-		for text, in cursor:
-			try:
-				f.write(text)
-				f.write(unicode("\n"))
-			except UnicodeEncodeError, e:
-				n_encoding_errors += 1
-			n_review += 1
-	return n_encoding_errors, n_review
+    n_encoding_errors = 0
+    n_review = 0
+    with io.open("reviewtext/%s.txt" % cat, 'w', encoding='utf8') as f:
+        for text, in cursor:
+            try:
+                f.write(text)
+                f.write(unicode("\n"))
+            except UnicodeEncodeError, e:
+                n_encoding_errors += 1
+            n_review += 1
+    return n_encoding_errors, n_review
 
 def main():
-	cnx = mysql.connector.connect(user='root', password='gottagofast',
-	                              host='127.0.0.1',
-	                              database='yelp_db')
+    cnx = mysql.connector.connect(user='root', password='gottagofast',
+                                  host='127.0.0.1',
+                                  database='yelp_db')
 
-	cursor = cnx.cursor()
-	cat = "Sushi Bars"
-	n_errors, n_total = write_document(cursor, cat)
-	print("%s: %d errors, %d total" % (cat, n_errors, n_total))
-	cursor.close()
-	cnx.close()
+    cursor = cnx.cursor()
+    cat = "Sushi Bars"
+    n_errors, n_total = write_document(cursor, cat)
+    print("%s: %d errors, %d total" % (cat, n_errors, n_total))
+    cursor.close()
+    cnx.close()
 
 if __name__ == '__main__':
-	main()
+    main()
