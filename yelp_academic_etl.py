42856db5b8dd717475177333f1f235f4ed1422b0
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-05 12:34:38 -0500

    yelp academic etl on single categories as documents

---
+++ yelp_academic_etl.py
@@ -0,0 +1,38 @@
+import mysql.connector
+# from sklearn.feature_extraction.text 
+
+
+def write_document(cursor, cat):
+	""" Given a yelp category, build out a text document
+	which has all the reviews for that category """
+	query = ("SELECT review.text "
+			 "FROM review INNER JOIN category ON review.business_id = category.business_id "
+			 "WHERE category.category=%s")
+	cursor.execute(query, (cat,))
+
+	n_encoding_errors = 0
+	n_review = 0
+	with open("reviewtext/%s.txt" % cat, 'w') as f:
+		for text, in cursor:
+			try:
+				f.write(text)
+				f.write('\n')
+			except UnicodeEncodeError, e:
+				n_encoding_errors += 1
+			n_review += 1
+	return n_encoding_errors, n_review
+
+def main():
+	cnx = mysql.connector.connect(user='root', password='gottagofast',
+	                              host='127.0.0.1',
+	                              database='yelp_db')
+
+	cursor = cnx.cursor()
+	cat = "Korean"
+	n_errors, n_total = write_document(cursor, cat)
+	print("%s: %d errors, %d total" % (cat, n_errors, n_total))
+	cursor.close()
+	cnx.close()
+
+if __name__ == '__main__':
+	main()
