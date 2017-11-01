676e505d3e151b36bb65202541714a51f227a3d4
youralien <ryanlouie2021@u.northwestern.edu>
2017-10-31 19:40:55 -0500

    WIP on using synonyms for expansion

--- affordance_language.py
+++ affordance_language.py
@@ -35,6 +35,7 @@ def natlang2keywords(aff):
     keywords: list of str
         i.e. ['downtown', 'riding', 'bike']
     """
+
     words = aff.strip().lower().split(' ')
     regex = re.compile('[^a-z]')
     keywords = []
@@ -129,6 +130,7 @@ def builtin_legos():
         obj1 = {}
         obj1["affordance.   "] = query
         obj1["keywords      "] = keywords
+        pprint(obj1)
 
         expand_keywords = True
         if expand_keywords:
@@ -143,7 +145,8 @@ def builtin_legos():
                     for sd_kw in second_degree_kws:
                         keywords.add(sd_kw)
 
-        obj1["expanded words"] = keywords
+        obj2 = {}
+        obj2["expanded words"] = keywords
 
         legos = set()
         for kw in keywords:
@@ -151,10 +154,116 @@ def builtin_legos():
                 if kw in word_assoc:
                     legos.add(lego)
 
-        obj1["legos         "] = legos
+        obj2["legos         "] = legos
+        pprint(obj2)
+
+        query = raw_input("Type another affordance query:\n")
+
+
+def builtin_legos_syn():
+
+    print("Loading...")
+    from lego_word_associations import lego_words
+    from oxford import synonyms
+
+    # EMBEDDING = KeyedVectors.load_word2vec_format('wiki.en/wiki.en.vec',
+    #                                               limit=100000)
+    affs = preload_affordances("storytime_affordances.txt")
+    shuffle(affs)
+    affs_generator = cycle(affs)
+
+    query = raw_input("Type a natural language affordance requirement:\n")
+    while (query != 'q'):
+
+        if query == '':
+            query = next(affs_generator)
+
+        keywords = set(natlang2keywords(query))
+        obj1 = {}
+        obj1["affordance.   "] = query
+        obj1["keywords      "] = keywords
         pprint(obj1)
+
+        expand_keywords = True
+        if expand_keywords:
+            for og_kw in keywords.copy():
+                first_degree_kws = synonyms(og_kw)
+
+                for fd_kw in first_degree_kws:
+                    keywords.add(fd_kw)
+
+        obj2 = {}
+        obj2["expanded words"] = keywords
+
+        legos = set()
+        for kw in keywords:
+            for lego, word_assoc in lego_words.iteritems():
+                if kw in word_assoc:
+                    legos.add(lego)
+
+        obj2["legos         "] = legos
+        pprint(obj2)
+
+        query = raw_input("Type another affordance query:\n")
+
+
+def builtin_legos_syn_emb():
+
+    print("Loading...")
+    from lego_word_associations import lego_words
+    from oxford import synonyms
+
+    EMBEDDING = KeyedVectors.load_word2vec_format('wiki.en/wiki.en.vec',
+                                                  limit=100000)
+    affs = preload_affordances("storytime_affordances.txt")
+    shuffle(affs)
+    affs_generator = cycle(affs)
+
+    query = raw_input("Type a natural language affordance requirement:\n")
+    while (query != 'q'):
+
+        if query == '':
+            query = next(affs_generator)
+
+        keywords = set(natlang2keywords(query))
+        obj1 = {}
+        obj1["affordance.   "] = query
+        obj1["keywords      "] = keywords
+        pprint(obj1)
+
+        expanded_keywords = set()
+        for og_kw in keywords:
+            first_degree_kws = synonyms(og_kw)
+
+            for fd_kw in first_degree_kws:
+                expanded_keywords.add(fd_kw)
+
+        obj2 = {}
+        obj2["expanded words"] = expanded_keywords
+
+        refined_keywords = set()
+        for candidate in expanded_keywords:
+            use = True
+            for word in keywords:
+                try:
+                    if (EMBEDDING.similarity(candidate, word) < 0.25):
+                        use = False
+                except KeyError:  # word not in vocab
+                    use = False
+            if use:
+                refined_keywords.add(candidate)
+
+        obj2["refined words"] = refined_keywords
+        legos = set()
+        for kw in refined_keywords:
+            for lego, word_assoc in lego_words.iteritems():
+                if kw in word_assoc:
+                    legos.add(lego)
+
+        obj2["z: legos         "] = legos
+        pprint(obj2)
 
         query = raw_input("Type another affordance query:\n")
 
 if __name__ == '__main__':
-    builtin_legos()
+    builtin_legos_syn()
