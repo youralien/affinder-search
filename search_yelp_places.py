13b996714ef6051c0af1840960a095f212da8ce5
youralien <ryanlouie2021@u.northwestern.edu>
2017-11-02 17:02:06 -0400

    output places as json

--- search_yelp_places.py
+++ search_yelp_places.py
@@ -6,6 +6,7 @@ Usage:
 python search_yelp_places.py "someone who is walking their dog"
 
 """
+import json
 from affordance_language import natlang2keywords
 from yelp_academic_etl import load_tfidf, query_categories_by_many
 
@@ -34,8 +35,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # TODO(rlouie): edit output to be handled by meteor command
-    # See meteor calling python script example:
-    # https://github.com/NUDelta/pair-research-meteor/blob/9536ec12d03f339d75fdc9d6dda8329dbf0cdc13/imports/api/pairings/methods.js#L94
-
-    print search_yelp_places_that_afford(args.query)
+    place_categories = search_yelp_places_that_afford(args.query)
+    place_categories = [elem.encode('ascii', 'ignore')
+                        for elem in place_categories]
+    print(json.dumps(place_categories))
