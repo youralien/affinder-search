b570f7368100490d6a05880584d16babf1424c7c
youralien <ryanlouie2021@u.northwestern.edu>
2020-12-12 12:42:04 -0800

    clean search that just describes descriptions

--- elasticsearch/search-reviews-app.py
+++ elasticsearch/search-reviews-app.py
@@ -11,6 +11,21 @@ def search(query):
     resp = client.search(index='yelp-reviews-small', body={"query": {"match": {"text": query }}})
     return jsonify(resp)
 
+@app.route("/cleansearch/<string:query>/")
+def cleansearch(query):
+    query = query.replace('+', ' ')
+    resp = client.search(index='yelp-reviews-small', body={"query": {"match": {"text": query }}})
+    
+    # text is in resp[hits][hits][i][_source][text]
+    # score is in resp[hits][hits][i][_score]
+
+    texts = []
+    for hit in resp["hits"]["hits"]:
+        text = hit["_source"]["text"]  
+        score = hit["_score"]
+        texts.append(text)
+
+    return jsonify(texts)
 
 if __name__ == '__main__':
     app.run(port=8080, host='localhost')
